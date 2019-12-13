70ddff16e3dd6e699424b9c9a6ed0d0c623f7de7
KunnyS3822 <kunnys3822@gmail.com>
2019-12-13 14:02:14 +0800

    YEAHHHHH

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function showTime(){
+    let date = new Date();
+    let hours = date.getHours();//0-23
+    let minutes = date.getMinutes()//0-59
+    let getSeconds = date.getSeconds()//0-59
+
+let formatHours = convertformat(hours)
+}
+
+function convertFormat(time){
+
+}
